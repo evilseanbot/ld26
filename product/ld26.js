cc7162ce83fed5281e0e47605035f079451cf9cb
Sean <seanmusgraveit@gmail.com>
2013-04-27 15:40:12 -0700

    Made blue & red lights work

--- product/ld26.js
+++ product/ld26.js
@@ -45,9 +45,13 @@ Crafty.c("Lightable", {
     reLight: function() {
 	    var light = 0;
 	    for (var i = 0; i < Crafty("LightSource").length; i++) {
-		    var torch = Crafty(Crafty("LightSource")[i]);
-			var distance = Math.abs(this.x - torch.x) + Math.abs(this.y - torch.y);
-			light += 800000.0 / Math.pow(distance, 3);
+		    var source = Crafty(Crafty("LightSource")[i]);
+			if (source.lit) {
+			    if ((source.lightColor == "red" && this.has("red")) || (source.lightColor == "blue" && this.has("blue"))) {
+    				var distance = Math.abs(this.x - source.x) + Math.abs(this.y - source.y);
+	    			light += 800000.0 / Math.pow(distance, 3);
+			    }
+			}
 		}
 		if (light > 0.25) {
 		    this.alpha = light;
@@ -58,9 +62,11 @@ Crafty.c("Lightable", {
 });
 
 Crafty.c("LightSource", {
+    lit: true,
+	lightColor: "red",
     lightUp: function() {
-	    for (var i = 0; i < Crafty("red").length; i++) {	 			
-		    Crafty(Crafty("red")[i]).reLight();
+	    for (var i = 0; i < Crafty(this.lightColor).length; i++) {	 			
+		    Crafty(Crafty(this.lightColor)[i]).reLight();
 		}
 	}    
 });
@@ -68,113 +74,76 @@ Crafty.c("LightSource", {
 Crafty.c("Torch", {
     init: function() {
 	    this.attr({h: 30, w: 30});
-	    this.color("#FF0000");
 	}
 }) 
 
+Crafty.c("MouseScreen", {
+    init: function() {
+	    this.bind('MouseDown', function(e) {
+		    var cursor = Crafty("Cursor");
+		
+			if(e.mouseButton == Crafty.mouseButtons.LEFT) {
+			    if (cursor.mode == 0) {
+					var torch = Crafty.e("2D, Canvas, Color, LightSource, Torch").attr({x: cursor.x, y: cursor.y});
+					torch.color("#FF4444");
+					torch.lightColor = "red";
+					torch.lightUp();
+				} else if (cursor.mode == 1) {   				    
+				    console.log(cursor.hit("Torch")[0]["obj"][0]);
+				    cursor.hit("Torch")[0]["obj"].destroy();
+				} else if (cursor.mode == 2) {
+					var torch = Crafty.e("2D, Canvas, Color, LightSource, Torch").attr({x: cursor.x, y: cursor.y});
+					torch.color("#4444FF");
+					torch.lightColor = "blue";
+					torch.lightUp();				
+				}
+				
+			} else if (e.mouseButton == Crafty.mouseButtons.RIGHT) {
+			    cursor.changeMode();
+			}
+        });
+		
+		this.bind('MouseMove', function() {
+		    var cursor = Crafty("Cursor");		
+		    cursor.lightUp();
+		});
+	}
+});
+
 Crafty.c("Cursor", {
+    mode: 0,
+	modes: 3,
     getAngle: function() {
 	    var deltaY = this.y - Crafty("Player").y;
 		var deltaX = this.x - Crafty("Player").x;
         var angleInDegrees = Math.atan2(deltaY, deltaX) * 180 / Math.PI	
 		return angleInDegrees;
 	},
+	changeMode: function() {
+	    this.mode++;
+		if (this.mode >= this.modes) {
+		    this.mode = 0;
+		}
+		
+		if (this.mode == 0) {
+		    this.color("#FF0000");
+			this.lit = true;
+			this.lightColor = "red";
+		} else if (this.mode == 1) {
+		    this.color("#FFFFFF");
+			this.lit = false;
+		} else if (this.mode ==2) {
+		    this.color("#0000FF");
+			this.lit = true;
+			this.lightColor = "blue";
+		}
+	},
     init: function() {
 	    this.bind("EnterFrame", function() {
 		    this.attr({x: mouseX-10, y: mouseY-10});
 		});
-		
-		this.bind('MouseDown', function(e) {
-		    console.log(e);
-			if 
-            var torch = Crafty.e("2D, Canvas, Color, LightSource, Torch").attr({x: this.x, y: this.y});
-			torch.lightUp();
-        })
-		
-		this.bind('MouseMove', function() {
-		    this.lightUp();
-		})
 	}
 });
-
-/*
-Crafty.c("Player", {
-    init: function() {
-	    this.bind("EnterFrame", function() {
-			redTimer -= 0.01;
-			
-			if (redTimer <= 0.01) {
-				redOn = false;
-			}
-			
-			if (redOn) {
-				for (var i = 0; i < Crafty("red").length; i++) {	 			
-					Crafty(Crafty("red")[i]).attr({alpha:(1.0 * redTimer)});				
-				}
-			} else {
-				for (var i = 0; i < Crafty("red").length; i++) {	 			
-					Crafty(Crafty("red")[i]).attr({alpha:0});				
-				}				
-			}
-		});
-	
-	    this.bind("KeyDown", function(e) {
-		    console.log(e);
-			
-			if (e.key == 90) {
-			   var newBullet = Crafty.e("2D, Canvas, Collision, Bullet, Color")
-			       .color("#FFFFFF")
-				   .attr({x: this.x, y:this.y, h: 10, w: 10});
-				   
-			   newBullet.xSpeed = Math.cos(Crafty("Cursor").getAngle()/ 57.2957795)*20;
-			   newBullet.ySpeed = Math.sin(Crafty("Cursor").getAngle()/ 57.2957795)*20;
-			}
-			
-			if (e.key == 49) {
-				
-			    if (redOn) {
-					redOn = false;
-				} else {
-					redOn = true;
-					redTimer = 1;
-				}
-			}
-
-			if (e.key == 50) {
-				console.log("Switching green");
-			    if (greenOn) {
-				    for (var i = 0; i < Crafty("green").length; i++) {
-					    Crafty(Crafty("green")[i]).attr({alpha:0.4});
-					}
-					greenOn = false;
-				} else {
-				    for (var i = 0; i < Crafty("green").length; i++) {
-					    Crafty(Crafty("green")[i]).attr({alpha:0});
-					}
-					greenOn = true;
-				}
-			}
-
-			if (e.key == 51) {
-				console.log("Switching green");
-			    if (blueOn) {
-				    for (var i = 0; i < Crafty("blue").length; i++) {
-					    Crafty(Crafty("blue")[i]).attr({alpha:0.6});
-					}
-					blueOn = false;
-				} else {
-				    for (var i = 0; i < Crafty("blue").length; i++) {
-					    Crafty(Crafty("blue")[i]).attr({alpha:0});
-					}
-					blueOn = true;
-				}
-			}
-			
-			
-		})
-	}
-});
-*/
 
 $(document).ready(function() {
 	$.ajaxSetup({
@@ -215,6 +184,10 @@ $(document).ready(function() {
 	Crafty.sprite(64, 64, "brickWall.png", {
 	    brickWall: [0, 0]
 	});
+
+	Crafty.sprite(64, 64, "sky.png", {
+	    sky: [0, 0]
+	});
 	
 
     Crafty.init(screenWidth, screenHeight);
@@ -224,7 +197,7 @@ $(document).ready(function() {
 	//Crafty.e("2D, Canvas, Color, redTorch, Torch").attr({h: 50, w: 50, x: 600, y:500}).color("#FF0000");
 	
 	//Crafty.e("2D, Canvas, Color, Multiway, Player").attr({h: 50, w: 50, x: 50, y:400}).color("#0000FF").multiway({x:10, y:10}, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180});
-	Crafty.e("2D, Canvas, Color, Mouse, LightSource, Cursor").attr({h: 20, w: 20, x: 20, y:20}).color("#FF0000");
+	Crafty.e("2D, Canvas, Color, Collision, LightSource, Cursor").attr({h: 20, w: 20, x: 20, y:20}).color("#FF0000");
 
 	
 	Crafty.e("2D, Canvas, Sprite, redFace, red, Lightable").attr({x: 400, y:200, alpha: 0});	
@@ -271,8 +244,28 @@ $(document).ready(function() {
 	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 10*64, y:5*64, alpha: 0});
 	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 10*64, y:4*64, alpha: 0});
 	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 10*64, y:3*64, alpha: 0});
-	
-	
+
+	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 6*64, y:8*64, alpha: 0});
+	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 7*64, y:8*64, alpha: 0});
+	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 8*64, y:8*64, alpha: 0});
+	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 9*64, y:8*64, alpha: 0});
+	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 10*64, y:8*64, alpha: 0});
+	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 11*64, y:8*64, alpha: 0});
+	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 12*64, y:8*64, alpha: 0});
+	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 13*64, y:8*64, alpha: 0});
+	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 14*64, y:8*64, alpha: 0});
+	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 15*64, y:8*64, alpha: 0});
+	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 16*64, y:8*64, alpha: 0});
+	Crafty.e("2D, Canvas, Sprite, brickWall, red, Lightable").attr({x: 17*64, y:8*64, alpha: 0});	
 	
 	Crafty.e("2D, Canvas, Sprite, redBlood, red, Lightable").attr({x: 400, y:200, alpha: 0});
+	
+	for (var mapX = 0; mapX < 18; mapX++) {
+	    for (var mapY = 0; mapY < 2; mapY++) {
+		    Crafty.e("2D, Canvas, Sprite, sky, blue, Lightable").attr({x: mapX*64, y: mapY*64, alpha: 0});
+		}
+	}
+	
+	
+	Crafty.e("2D, Canvas, Mouse, MouseScreen").attr({x: 0, y: 0, h: screenHeight, w:screenWidth});
 });
